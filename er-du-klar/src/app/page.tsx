108593ea18fe1fc56a37344847775ab9a9ef061f
jonous10 <jonous10@imkatta.no>
2025-05-12 15:11:43 +0200

    Fix errors

--- er-du-klar/src/app/page.tsx
+++ er-du-klar/src/app/page.tsx
@@ -1,12 +1,16 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
 import Header from "@/components/ui/header";
-import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import dataTEST from "@/data-test/data.json"
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [language, setLanguage] = useState<string>("en")
   return (
     <div>
-      <Header/>
+      <Header lan={language}/>
     </div>
   );
 }

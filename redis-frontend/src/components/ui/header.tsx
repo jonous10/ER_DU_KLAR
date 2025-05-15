a8c1bb1aa3d8b90e52df48174ea0774211ef7537
jonous10 <jonous10@imkatta.no>
2025-05-15 15:02:58 +0200

    Update header

--- redis-frontend/src/components/ui/header.tsx
+++ redis-frontend/src/components/ui/header.tsx
@@ -3,9 +3,10 @@
 import Link from "next/link";
 import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from "./navigation-menu";
 import { Separator } from "./separator";
-import { AccountCircleIcon, HomeIcon, SettingsIcon } from "@/components/ui/icons"
 import { SettingsPopover } from "./settings-popover";
 import { AccountPopover } from "./account-popover";
+import image from "@/images/politiet.png"
+import Image from "next/image";
 
 
 interface HeaderProps {
@@ -16,23 +17,26 @@ export default function Header({ lan }: HeaderProps) {
         <div>
             <NavigationMenu className="p-2">
                 <div className="w-screen">
-                    <NavigationMenuList>
+                    <NavigationMenuList className="flex flex-col sm:flex-row">
                         <NavigationMenuItem>
                             <Link
                             href="http://localhost:3000"
                             legacyBehavior passHref
                             >
                                 <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                    <HomeIcon/>
+                                    <Image 
+                                        className="w-40"
+                                        src={image} alt={""}/>
                                 </NavigationMenuLink>
                             </Link>
                         </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <AccountPopover/>
-                        </NavigationMenuItem>
+
+                        
+
                         <NavigationMenuItem className="ml-auto mr-0">
                             <div className="flex flex-row gap-4">
                                 <SettingsPopover/>
+                                <AccountPopover/>
                             </div>
                         </NavigationMenuItem>
                     </NavigationMenuList>

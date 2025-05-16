b9fa76178931d6aaf79e9835e211d1775aacd77a
jonous10 <jonous10@imkatta.no>
2025-05-16 09:05:44 +0200

    Init language selector

--- redis-frontend/src/app/page.tsx
+++ redis-frontend/src/app/page.tsx
@@ -1,19 +1,17 @@
 "use client"
 
 import Header from "@/components/ui/header";
+import { LanguageSelector } from "@/components/ui/language-select";
 import language_sup from "@/language-sup/language_sup.json"
-
-// pages/index.tsx
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [lang, setLang] = useState<string>("en");
+  const [lang, setLang] = useState("")
 
-  // Fetch selected language from memory storage
   useEffect(() => {
     const saved = localStorage.getItem("lang");
-    if (saved) setLang(saved);
-  }, []);
+    setLang(saved ?? "en");
+  }, [])
 
   // This langUpdate() will only be used in the language selector later
   const langUpdate = (input: string) => {
@@ -24,8 +22,21 @@ export default function Home() {
   return (
     <div>
       <Header/>
-      <button onClick={() => window.location.href = "/chapter-1/page.tsx"}>{language_sup.start_course[lang as keyof typeof language_sup.start_course] || "Language unsupported"}</button>
-      <input onChange={(e) => langUpdate(e.target.value)}></input>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <h1 className="text-9xl">
+          {language_sup.ARE_YOU_READY[lang as keyof typeof language_sup.start_course]}
+        </h1>
+      </div>
+
+      <LanguageSelector updateLanguage={langUpdate}/>
+      
+      <button onClick={() => window.location.href = "/chapter-1/page.tsx"}>
+        {language_sup.start_course[
+          lang as keyof typeof language_sup.start_course
+        ] 
+        || "Language unsupported"}
+      </button>
+
     </div>
   );
 }
---
+++ redis-frontend/src/components/ui/language-select.tsx
@@ -0,0 +1,28 @@
+import * as React from "react"
+
+import { ScrollArea } from "@/components/ui/scroll-area"
+import language_sup from "@/language-sup/language_sup.json"
+import { Button } from "./button"
+
+
+interface LanguageSelectorProps {
+    updateLanguage: ( lang: string ) => void;
+}
+export function LanguageSelector({updateLanguage}: LanguageSelectorProps) {
+  return (
+    <ScrollArea className="h-72 w-22 rounded-md border">
+      <div className="p-4">
+        {language_sup.supports.map((lang) => {
+            return (
+                <div key={lang} className="flex items-center justify-center">
+                    <p
+                    className="m-1 w-12 h-8 cursor-pointer hover:bg-gray-100 rounded-2xl text-2xl flex items-center justify-center text-center"
+                    onClick={() => updateLanguage(lang)}
+                    >{lang}</p>
+                </div>
+            )
+        })}
+      </div>
+    </ScrollArea>
+  )
+}

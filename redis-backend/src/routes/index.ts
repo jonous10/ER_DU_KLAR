12ed6f79d95ab35112a930465511a251b3510ae7
jonous10 <jonous10@imkatta.no>
2025-05-16 13:55:18 +0200

    Remove Language API and Routes

--- redis-backend/src/routes/index.ts
+++ redis-backend/src/routes/index.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import Users from "./users";
-import Language from "./language";
 import userRequireMiddleware from "../middleware/userRequire";
 
 const router = Router();
 
 router.use(userRequireMiddleware);
 router.use("/users", Users);
-router.use("/language", Language);
 
 export default router;
--- redis-frontend/src/app/page.tsx
+++ redis-frontend/src/app/page.tsx
@@ -3,6 +3,7 @@
 import Header from "@/components/ui/header";
 import language_sup from "@/language-sup/language_sup.json"
 import { useLanguage } from "@/contexts/language-context";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const { lang, setLang } = useLanguage();
@@ -11,17 +12,22 @@ export default function Home() {
     <div>
       <Header/>
       <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <h1 className="text-9xl">
+        <h1 className="text-3xl sm:text-7xl">
           {language_sup.ARE_YOU_READY[lang as keyof typeof language_sup.start_course]}
         </h1>
       </div>
       
-      <button onClick={() => window.location.href = "/chapter-1/page.tsx"}>
-        {language_sup.start_course[
-          lang as keyof typeof language_sup.start_course
-        ] 
-        || "Language unsupported"}
-      </button>
+      <div className="flex justify-center m-40">
+        <Button
+          className=""
+          onClick={() => window.location.href = "/chapter-1/page.tsx"}
+        >
+          {language_sup.start_course[
+        lang as keyof typeof language_sup.start_course
+          ] 
+          || "Language unsupported"}
+        </Button>
+      </div>
 
     </div>
   );

c3e5211fbaf5a24194000e430997fa44a9126e5a
jonous10 <jonous10@imkatta.no>
2025-05-20 08:31:57 +0200

    develop chapter-2

--- redis-frontend/src/app/chapter-2/page.tsx
+++ redis-frontend/src/app/chapter-2/page.tsx
@@ -4,9 +4,7 @@ import Header from "@/components/ui/header";
 import language_sup from "@/language-sup/language_sup.json"
 import { useLanguage } from "@/contexts/language-context";
 import { Card } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
 import { useState } from "react";
-import { Separator } from "@radix-ui/react-separator";
 
 export default function Home() {
   const { lang, setLang } = useLanguage();
@@ -18,7 +16,15 @@ export default function Home() {
       <div className="flex flex-col justify-center items-center m-10 mt-40">
         
         <Card className="max-w-180 p-8">
-          <p className="text-3xl">{language_sup[lang as keyof typeof language_sup]}</p>
+          <p className="text-3xl">{language_sup.test_failed_again[lang as keyof typeof language_sup.test_failed_again]}</p>
+        </Card>
+        <Card className="max-w-180 p-8">
+          <div
+            className="text-3xl"
+            dangerouslySetInnerHTML={{
+              __html: language_sup.trust_worthy_example[lang as keyof typeof language_sup.trust_worthy_example],
+            }}
+          />
         </Card>
       </div>
     </div>

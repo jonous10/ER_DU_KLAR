a0e2cb0d8262b87285a37786f74d76f68c5c9e80
jonous10 <jonous10@imkatta.no>
2025-05-20 09:16:12 +0200

    Add more text & navigation

--- redis-frontend/src/app/chapter-2/page.tsx
+++ redis-frontend/src/app/chapter-2/page.tsx
@@ -8,24 +8,47 @@ import { useState } from "react";
 
 export default function Home() {
   const { lang, setLang } = useLanguage();
-  const [failed, setFailed] = useState<boolean>(false);
+  const [next, setNext] = useState<boolean>(false);
 
   return (
     <div>
       <Header/>
       <div className="flex flex-col justify-center items-center m-10 mt-40">
-        
-        <Card className="max-w-180 p-8">
-          <p className="text-3xl">{language_sup.test_failed_again[lang as keyof typeof language_sup.test_failed_again]}</p>
-        </Card>
-        <Card className="max-w-180 p-8">
+        { next ? 
+        <div>
+          <Card className="max-w-240 p-8">
+            <div
+              className="text-3xl"
+              dangerouslySetInnerHTML={{
+                __html: language_sup.trust_worthy_example[lang as keyof typeof language_sup.trust_worthy_example],
+              }}
+            />
+            
+          </Card>
+          
+        </div>
+        :
+        <><Card className="max-w-240 p-8">
           <div
-            className="text-3xl"
-            dangerouslySetInnerHTML={{
-              __html: language_sup.trust_worthy_example[lang as keyof typeof language_sup.trust_worthy_example],
+              className="sm:text-4xl text-1xl"
+              dangerouslySetInnerHTML={{
+                __html: language_sup.test_failed_again[lang as keyof typeof language_sup.test_failed_again],
+              }}
+            />
+          <a
+            type="button"
+            title={language_sup.dont_worry[lang as keyof typeof language_sup.dont_worry]}
+            className="mt-4 text-blue-600 underline hover:cursor-pointer cursor-pointer px-4 py-2 rounded m-auto"
+            onClick={() => {
+              alert("Don't worry, this is actually safe this time")
+              setNext(true)
             }}
-          />
+          >
+            {language_sup.next[lang as keyof typeof language_sup.next]}
+          </a>
         </Card>
+        </>
+        }
       </div>
     </div>
   );
